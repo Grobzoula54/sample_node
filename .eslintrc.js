b90505af043c2281cdabad03575fec7fa7febcdf
Grobzoula54 <gregoirenoel54@gmail.com>
2021-06-14 13:47:21 +0200

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+/*module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};
+*/
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 12
+    },
+    "ignorePatterns": ["*.spec.js"],
+    "rules": {
+
+    }
+};
